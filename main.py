225814103cce567510d1e0b2ce0ee0678323676a
rudalzu <rudalzu@gmail.com>
2025-05-16 21:04:29 -0500

    avanzamos hasta el tutorial hasta el minuto 26

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-"""
-magina que esta API es una biblioteca de peliculas:
-La funcion load_movies() es como una biblioteca que carga el catalogo de libros (peliculas) cuando se abre la biblioteca.
-La funcion get_movies() muestra todo el catalogo cuando alguien lo pide.
-La funcion get_movie(id) es como si alguien preguntara por un libro especifico es decir, por un coidgo de identificacion.
-La funcion chatbot (query) es un asistente que busca peliculas segun palabras clave y sinonimo.
-La funcion get_movies_by_category(category) ayuda a encontrar peliculas segun su genero (accion, comedia, etc...)
-"""
+''' 
+imagina que esta API es una biblioteca de peliculas
+La función load_movies() es como una biblioteca de libros (peliculas) cuando se abre la biblioteca
+La función get_movies() muestra todo el catalogo cuando alguien lo pide.
+La fucnción get_movie(id) es como si alguien preguntara por un libro especifico es decir, por un codigo de identificación
+La funcion chat bot (query) es un asistente que busca peliculas segun palabras clave y sinonimo.
+La función get_movies_by_category(category) ayuda a encontrar peliculas segun su genero (acción, comedia, etc...)
+'''
 
 # Importamos las herramientas necesarias para continuar nuestra API
-from fastapi import FastAPI, HTTPException # FastAPI nos ayuda a crear la API, HTTPException nos ayuda a manejar errores
-from fastapi.responses import HTMLResponse, JSONResponse # HTMLResponse nos ayuda a manejar respuestas HTML, JSONResponse nos ayuda a manejar respuestas en formato JSON
-import pandas as pd # pandas nos ayuda a manejar datos en tablas como si fuera un Excel
+from fastapi import FastAPI, HTTPException # FastAPI nos ayuda a crear la API, HTTPException nos ayuda a mejorar errores
+from fastapi.responses import HTMLResponse # HTMLResponse nos ayuda a mejorar respuestas HTML, JSONResponse nos ayuda a manejar respuestas
+import pandas as pd # pandas nos ayuda a manejar tablas como si fuera un Excel
 import nltk # nltk es una libreria para procesar texto y analizar palabras
 from nltk.tokenize import word_tokenize # word_tokenize nos ayuda a tokenizar texto, es decir, a convertirlo en palabras
 from nltk.corpus import wordnet # wordnet es una libreria para analizar sinonimos
 
-# indicamos la ruta donde nltk buscara los datos descargados en nuestro cumputador
-nltk.data.path.append('C:\Users\USER\AppData\Roaming\nltk_data')
-nltk.download('punkt')
+# Indicamos la ruta donde nltk buscara los datos descargados en nuestro computador
+nltk.data.path.append(r'C:\Users\USER\AppData\Roaming\nltk_data')
+nltk.download('punkt') # es un paquete para dividir frases en palabras
+nltk.download('wordnet') # paquete para encontrar sinonimos en palabras
+
+# función para cargar las peliculas desde un archivo csv
+
+def load_movies():
+    # leemos el archivo que contiene información de peliculas y selecciónamos las columnas mas importantes 
+    df = pd.read_csv("Dataset/netflix_titles.csv")[['show_id','title','release_year','listed_in','rating','description']]
+    
+    # Renombramos las colunmas para que sean mas faciles de entender
+    df.columns = ['id', 'title', 'year', 'category', 'rating', 'overview']
+    
+    # Llenamos los espacios vacios con texto vacio y convertimos los datos en una lista de diccionarios
+    return df.fillna('').to_dict(orient='records')
+
+# Cargamos las peliculas al iniciar la API para no leer el archivo cada vez que alguien pregunte por ellas
+movies_list = load_movies()
+
+# Función para encontrar sinonimos de una palabra
+def get_synonyms(word):
+    # Usamos wordnet para encontrar distintas palabras que significan lo mismo
+    return{lemma.name().lower() for syn in wordnet.synsets(word) for lemma in syn.lemmas()}
+
+# Creamos la aplicación FastAPI, que será el motor de nuestra API
+# Esto inicializa la API con una versión
